8ee09dcdc1f430783d4ac501050310a3f06d85d9
harkirt <harryhansra1@gmail.com>
2022-03-30 14:32:50 +0530

    changes by y

---
+++ Demo/src/test/Test3.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String y="this class created by y";
+ System.out.println(y);
+	}
+
+}
