64bde61e156cad01b41e84c4213998e83fa47d04
hakirt-singh <harryhansra1@gmail.com>
2022-03-30 10:45:35 +0530

    save

---
+++ Demo/src/test/Test2.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String tst="hello Harry";
+      if(tst.contains("Harry")) {
+    	  System.out.println(tst);
+      }
+	}
+
+}
